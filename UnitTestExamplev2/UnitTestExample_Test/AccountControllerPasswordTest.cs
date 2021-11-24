ffc56da9b57e2e4f98e849533c58208ad8d1f5b2
Brigitta Tóth <tbrigus@gmail.com>
2021-11-24 16:53:28 +0100

    5

---
+++ UnitTestExamplev2/UnitTestExample_Test/AccountControllerPasswordTest.cs
@@ -0,0 +1,33 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnitTestExample.Controllers;
+
+namespace UnitTestExample_Test
+{
+    public class AccountControllerPasswordTest
+    {
+        [
+         Test,
+         TestCase("abcd", false),
+         TestCase("CAPITAL", false),
+         TestCase("nocapital", false),
+         TestCase("Capital11", false),
+         TestCase("GoodPassword10", true)
+        ]
+        public void ValidatePassword(string password, bool expectedResult)
+        {
+            // Arrange
+            var accountController = new AccountController();
+
+            // Act
+            var actualResult = accountController.ValidatePassword(password);
+
+            // Assert
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+    }
+}
