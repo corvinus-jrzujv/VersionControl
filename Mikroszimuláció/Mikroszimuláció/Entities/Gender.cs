f20071b93a3cbec81b7765608e8412c39e6e7008
Brigitta Tóth <tbrigus@gmail.com>
2021-12-01 17:29:44 +0100

    1

---
+++ Mikroszimuláció/Mikroszimuláció/Entities/Gender.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mikroszimuláció.Entities
+{
+    public enum Gender
+    {
+        Male = 1,
+        Female = 2
+    }
+}
+
+    //// Példa enumerációs változók használatára
+    //Gender ádámNeme = Gender.Male;
+    //Gender évaNeme = (Gender)2;
+
+//if (ádámNeme == Gender.Male)
+//    {
+//...
+//}
+
